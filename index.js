0766ff9829581f7cdf1444e2da660bb1f0fba32a
Abhishek Maurya <166213264+Abhishekmaurya12367@users.noreply.github.com>
2026-03-11 22:56:23 +0530

    Add comment to root route handler
    Added comment to explain the root route handler.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {// the get methode help in providing the router so that we can route the code from the backend to the fronted;
   res.send('Hello World!')
 })
 app.get('\twitter',(req,res) => {
@@ -16,3 +16,4 @@ app.get('/login',(req,res)=>{
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
